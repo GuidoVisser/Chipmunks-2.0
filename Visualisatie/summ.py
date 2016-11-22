54d0777b1cba3420f1b44db3392b65ae26641c9c
LauraVrkmp <laura.veerkamp11@gmail.com>
2016-11-22 12:50:45 +0100

    Mappen

---
+++ Visualisatie/summ.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 11:31:50 2016
+
+@author: Laura
+"""
+from mpl_toolkits.mplot3d import proj3d
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+class Gates(object):
+    
+    def coords(self, x, y, z):
+        
